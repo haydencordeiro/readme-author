d5b22eeb0947d8249755a2de5aa75202602f23ae
Hayden Cordeiro <cordeirohayden@gmail.com>
2021-09-28 12:53:02 +0530

    Update script.py

--- script.py
+++ script.py
@@ -6,7 +6,7 @@ df.fillna('#', inplace=True)
 df = df.sort_values(by=['name'], ascending=True)
 
 for index, row in df.iterrows():
-    temp=f"""- {row["name"]}<br> 
+    temp=f"""- {row["name"].capitalize()}<br> 
 [![Linkedin](https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white)]({row["linkedin"]})
 [![Github](https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white)]({row["github"]})"""
     print(temp)
